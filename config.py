abd0e4f6ea5e5bd7ecb86bd24c95ed811114b510
Aman Khan <81584983+amansherjada@users.noreply.github.com>
2025-03-16 21:25:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # Set environment variables for Groq
-os.environ["GROQ_API_KEY"] = "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"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBg9zHkcZY1crkWeSu4D7MopolzEBhpj3E"
+os.environ["GROQ_API_KEY"] = "YOUR_GROQ_API_KEY"
+os.environ["GOOGLE_API_KEY"] = "YOUR_GOOGLE_API_KEY"
 os.environ["GROQ_MODEL_NAME"] = "llama-3.3-70b-versatile"  
